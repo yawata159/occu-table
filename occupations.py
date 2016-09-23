94117bf41c48e39453ccaba5da7ffbff60917514
AWinnerIsYou <seijiyawata@gmail.com>
2016-09-23 14:47:58 -0400

    Add basic struc

--- occupations.py
+++ occupations.py
@@ -2,9 +2,7 @@ from flask import Flask, render_template
 import string, random
 
 app = Flask(__name__)
-
-
-    
+   
 def job_dict():
     s = open("occupations.csv").read().strip().split("\r\n")
     d = dict()
@@ -14,22 +12,8 @@ def job_dict():
     
         value = line.rsplit(",",1)[1]
         d[key] = value
+    del d["Job Class"]
     return d
-
-def random_job():
-    copy = job_dict()
-    del copy["Job Class"]
-    del copy["Total"]
-    for key in copy:
-        copy[key] = float(copy[key])    
-    random_num = random.random()*100
-    threshold = 0.0
-    for key in copy:
-        threshold += copy[key]
-        if random_num < threshold:
-            return key
-    return "Other"
-
 
 @app.route("/")
 def intro():
@@ -37,9 +21,7 @@ def intro():
 
 @app.route("/occupations")
 def occupations():
-    job_dict = job_dict()
-    random_job = random_job()
-    return render_template('occupations.html')
+    return render_template('occupations.html', table = job_dict())
 
 if __name__ == '__main__':
     app.debug = True
