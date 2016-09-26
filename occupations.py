1f7710458dced9912291479ce015bcbe4f4e90e6
yawata159 <seijiyawata@gmail.com>
2016-09-25 20:31:31 -0400

    Add Links and Styling

--- occupations.py
+++ occupations.py
@@ -7,13 +7,31 @@ def job_dict():
     s = open("occupations.csv").read().strip().split("\r\n")
     d = dict()
     for line in s:
-        key = line.rsplit(",",1)[0]
+        key = line.rsplit(",",2)[0]
         key = string.strip(key,'\"') # remove double quotes
     
-        value = line.rsplit(",",1)[1]
+        value = line.rsplit(",",2)[1]
         d[key] = value
     del d["Job Class"]
+    del d["Total"]
+    for key in d:
+        d[key] = float(d[key])
     return d
+
+
+def link_dict():
+    s = open("occupations.csv").read().strip().split("\r\n")
+    d = dict()
+    for line in s:
+        key = line.rsplit(",",2)[0]
+        key = string.strip(key,'\"') # remove double quotes
+    
+        value = line.rsplit(",",2)[2]
+        d[key] = value
+    del d["Job Class"]
+    del d["Total"]
+    return d
+
 
 @app.route("/")
 def intro():
@@ -21,7 +39,22 @@ def intro():
 
 @app.route("/occupations")
 def occupations():
-    return render_template('occupations.html', table = job_dict())
+    # randomizer
+    d = job_dict()
+    l = link_dict()
+    pickJob = ''
+    random_num = random.random()*100
+    threshold = 0.0
+    for key in d:
+        threshold += d[key]
+        if random_num < threshold:
+            pickJob = key
+            break
+    if pickJob == '':
+        pickJob = "Other"
+    
+    # render
+    return render_template('occupations.html',table = d, links = l, rand = pickJob)
 
 if __name__ == '__main__':
     app.debug = True
